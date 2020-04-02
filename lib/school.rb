fca2472b2e09aed92c304ce677edc977eca3411d
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-02 01:08:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+
+def initialize(school)
+  @school = school
+  @roster = {}
+end
+
+def roster
+  @roster
+end
+
+def add_student(name,grade)
+  if !(@roster.include?(grade))
+    roster[grade]= Array.new
+  end
+  roster[grade].push(name)
+end
+
+def grade(grade)
+  roster[grade]
+end
+
+def sort 
+  @roster.each do |key,value|
+    @roster[key] = value.sort
+  end
+end
+
+
+end
